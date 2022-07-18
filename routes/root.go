d3a770b0a740b3a7fa5f4920fccae82687c79a6d
James Tombleson <luther38@gmail.com>
2022-07-18 15:03:33 -0700

    routes and templates have been updaed but still working on the flow

--- routes/root.go
+++ routes/root.go
@@ -12,10 +12,4 @@ func (s *HttpServer) Index(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (s *HttpServer) ArticleIndex(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content Type", "text/html")
-	err := s.templates.ExecuteTemplate(w, "articles.index", nil)
-	if err != nil {
-		panic(err)
-	}
-}
+
