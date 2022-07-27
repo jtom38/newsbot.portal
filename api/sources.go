59450fb54e19f59412f677b2c33f2623a3042443
James Tombleson <luther38@gmail.com>
2022-07-26 17:37:36 -0700

    Added call to find sources by source name

--- api/sources.go
+++ api/sources.go
@@ -31,6 +31,28 @@ func (c *ApiClient) ListSources() (*[]Source, error) {
 	return &items, nil
 }
 
+func (c *ApiClient) ListSourcesBySource(value string) (*[]Source, error) {
+	var items []Source
+
+	uri := fmt.Sprintf("%v/api/config/sources/by/source?source=%v", c.endpoint, value)
+	res, err := http.Get(uri)
+	if err != nil {
+		return &items, err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return &items, err
+	}
+
+	err = json.Unmarshal(body, &items)
+	if err != nil {
+		return &items, err
+	}
+
+	return &items, nil
+}
+
 func (c *ApiClient) GetSourceById(ID uuid.UUID) (*Source, error) {
 	var items Source
 
@@ -52,3 +74,4 @@ func (c *ApiClient) GetSourceById(ID uuid.UUID) (*Source, error) {
 
 	return &items, nil
 }
+
