de799aeb0948c9ea2dac6bc755d23d2f2de640bc
James Tombleson <luther38@gmail.com>
2022-07-30 08:46:29 -0700

    can create new reddit sources now

--- api/sources.go
+++ api/sources.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 
 	"github.com/google/uuid"
 )
@@ -17,6 +19,7 @@ func (c *ApiClient) ListSources() (*[]Source, error) {
 	if err != nil {
 		return &items, err
 	}
+	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
@@ -39,6 +42,7 @@ func (c *ApiClient) ListSourcesBySource(value string) (*[]Source, error) {
 	if err != nil {
 		return &items, err
 	}
+	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
@@ -61,6 +65,7 @@ func (c *ApiClient) GetSourceById(ID uuid.UUID) (*Source, error) {
 	if err != nil {
 		return &items, err
 	}
+	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
@@ -75,3 +80,16 @@ func (c *ApiClient) GetSourceById(ID uuid.UUID) (*Source, error) {
 	return &items, nil
 }
 
+func (c *ApiClient) SourceNewReddit(name string, sourceUrl string) error {
+	endpoint := fmt.Sprintf("%v/api/config/sources/new/reddit?name=%v&url=%v", c.endpoint, name, url.QueryEscape(sourceUrl))
+	res, err := http.Post(endpoint, "application/json", nil)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != 200 {
+		return errors.New("unexpected status code")
+	}
+
+	return nil
+}
--- routes/sources.go
+++ routes/sources.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -12,14 +13,12 @@ func (s *HttpServer) sourcesRouter() http.Handler {
 
 	r.Get("/", s.SourcesIndex)
 	r.Get("/reddit", s.SourcesRedditIndex)
+	r.Get("/reddit/new", s.SourcesRedditNewDisplay)
+	r.Post("/reddit/new/post", s.SourcesRedditNewPost)
+
 	r.Get("/youtube", s.SourcesyYouTubeIndex)
 	r.Get("/twitch", s.SourcesTwitchIndex)
 	r.Get("/ffxiv", s.SourcesFfxivIndex)
-
-
-	//r.Route("/{ID}", func(r chi.Router) {
-	//	r.Get("/", s.GetSourceById)
-	//})
 
 	return r
 }
@@ -27,6 +26,10 @@ func (s *HttpServer) sourcesRouter() http.Handler {
 type ListSourcesParam struct {
 	Title string
 	Subtitle string
+
+	// Defines what source is currently active.
+	// Used for routing
+	Source string
 	Items *[]api.Source
 }
 
@@ -42,6 +45,7 @@ func (s *HttpServer) SourcesRedditIndex(w http.ResponseWriter, r *http.Request) 
 	param := ListSourcesParam{
 		Title: "Reddit Sources",
 		Subtitle: "Here are the available sources.",
+		Source: "reddit",
 	}
 
 	items, err  := s.api.ListSourcesBySource("reddit")
@@ -57,6 +61,67 @@ func (s *HttpServer) SourcesRedditIndex(w http.ResponseWriter, r *http.Request) 
 	}
 }
 
+func (s *HttpServer) SourcesRedditNewDisplay(w http.ResponseWriter, r *http.Request) {
+	param := ListSourcesParam{
+		Title: "Reddit Sources",
+		Subtitle: "Here are the available sources.",
+		Source: "reddit",
+	}
+
+	items, err  := s.api.ListSourcesBySource("reddit")
+	if err != nil {
+		panic(err)
+	}
+	param.Items = items
+
+	w.Header().Add("Content Type", "text/html")
+	err = s.templates.ExecuteTemplate(w, "sources.new.reddit", param)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// This validates the infomation sent from the form and passes it to the API.
+func (s *HttpServer) SourcesRedditNewPost(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		s.templates.ExecuteTemplate(w, "err", ErrParam{
+			Title: "Form Error",
+			Code: 500,
+			Error: err,
+		})
+		return 
+	}
+
+	name := r.Form.Get("name")
+	if name == "" {
+		s.templates.ExecuteTemplate(w, "err", ErrParam{
+			Title: "Invalid Article ID",
+			Code: 500,
+			Error: err,
+		})
+		return 
+	}
+
+	uri := fmt.Sprintf("https://reddit.com/r/%v", name)
+	err = s.api.SourceNewReddit(name, uri)
+	if err != nil {
+		s.templates.ExecuteTemplate(w, "err", ErrParam{
+			Title: "Failed to add new Reddit source",
+			Code: 500,
+			Error: err,
+		})
+		return 
+	}
+
+	w.Header().Add("Content Type", "text/html")
+	err = s.templates.ExecuteTemplate(w, "sources.list", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+
 // This displays all the youtube sources known to the app.
 //
 // /settings/sources/youtube
@@ -64,6 +129,7 @@ func (s *HttpServer) SourcesyYouTubeIndex(w http.ResponseWriter, r *http.Request
 	param := ListSourcesParam{
 		Title: "YouTube Sources",
 		Subtitle: "Here are the available sources.",
+		Source: "YouTube",
 	}
 
 	items, err  := s.api.ListSourcesBySource("youtube")
@@ -86,6 +152,7 @@ func (s *HttpServer) SourcesTwitchIndex(w http.ResponseWriter, r *http.Request) 
 	param := ListSourcesParam{
 		Title: "Twitch Sources",
 		Subtitle: "Here are the available sources.",
+		Source: "twitch",
 	}
 
 	items, err  := s.api.ListSourcesBySource("twitch")
@@ -108,6 +175,7 @@ func (s *HttpServer) SourcesFfxivIndex(w http.ResponseWriter, r *http.Request) {
 	param := ListSourcesParam{
 		Title: "Final Fantasy XIV Sources",
 		Subtitle: "Here are the available sources.",
+		Source: "ffxiv",
 	}
 
 	items, err  := s.api.ListSourcesBySource("ffxiv")
