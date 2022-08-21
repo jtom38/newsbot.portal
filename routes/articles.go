aa3ef1aa818af7f8c6a7c9ca3b9cb216987e8ca7
James Tombleson <luther38@gmail.com>
2022-08-21 11:10:33 -0700

    Featues/init/articles newest (#16)
    * updated navbar to take you right to articles/newest
    
    * playing with the article list format some and added a query value to switch views

--- routes/articles.go
+++ routes/articles.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
 
@@ -15,6 +16,7 @@ func (s *HttpServer) articlesRouter() http.Handler {
 
 	r.Get("/", s.ArticleIndex)
 	r.Get("/list", s.ListArticles)
+	r.Get("/newest", s.ListArticles)
 
 	r.Route("/{ID}", func(r chi.Router) {
 		r.Get("/", s.DisplayArticleById)
@@ -24,7 +26,7 @@ func (s *HttpServer) articlesRouter() http.Handler {
 	r.Route("/sources/{ID}", func(r chi.Router) {
 		r.Get("/list", s.ListArticlesBySource)
 	})
-	
+
 	//r.Get("/source", s.ListArticlesBySourceId)
 
 	return r
@@ -32,26 +34,26 @@ func (s *HttpServer) articlesRouter() http.Handler {
 
 type ErrParam struct {
 	Title string
-	Code int
+	Code  int
 	Error error
 }
 
 // This struct contains extra details not exposed by the API
 type ApiSourceOverload struct {
-	Item api.Source
+	Item   api.Source
 	Topics []string
 }
 
 type ArticleIndexParam struct {
-	Title string
+	Title    string
 	Subtitle string
 }
 
 // /articles
 func (s *HttpServer) ArticleIndex(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content Type", "text/html")
-	param := ArticleIndexParam {
-		Title: "Articles",
+	param := ArticleIndexParam{
+		Title:    "Articles",
 		Subtitle: "Placeholder",
 	}
 	err := s.templates.ExecuteTemplate(w, "articles.index", param)
@@ -61,12 +63,12 @@ func (s *HttpServer) ArticleIndex(w http.ResponseWriter, r *http.Request) {
 }
 
 type DisplayArticleParams struct {
-	Title string
+	Title    string
 	Subtitle string
-	Article *api.Article
-	Source *api.Source
-	Topics []string
-	IsImage bool
+	Article  *api.Article
+	Source   *api.Source
+	Topics   []string
+	IsImage  bool
 }
 
 func (s *HttpServer) DisplayArticleById(w http.ResponseWriter, r *http.Request) {
@@ -77,29 +79,29 @@ func (s *HttpServer) DisplayArticleById(w http.ResponseWriter, r *http.Request) 
 	if err != nil {
 		s.templates.ExecuteTemplate(w, "err", ErrParam{
 			Title: "Invalid ID",
-			Code: 500,
+			Code:  500,
 			Error: err,
 		})
 		return
 	}
-	
-	article, err  := s.api.Articles.Get(uuid)
+
+	article, err := s.api.Articles.Get(uuid)
 	if err != nil {
 		s.templates.ExecuteTemplate(w, "err", ErrParam{
 			Title: "Invalid Article ID",
-			Code: 404,
+			Code:  404,
 			Error: err,
 		})
-		return 
+		return
 	}
 	param.Article = article
 	param.Title = article.Title
-	
+
 	source, err := s.api.Sources.GetById(article.Sourceid)
 	if err != nil {
 		s.templates.ExecuteTemplate(w, "err", ErrParam{
 			Title: "Invalid Source ID",
-			Code: 500,
+			Code:  500,
 			Error: err,
 		})
 		return
@@ -111,8 +113,8 @@ func (s *HttpServer) DisplayArticleById(w http.ResponseWriter, r *http.Request) 
 	var topics []string
 	articleTags := strings.Split(article.Tags, ",")
 	sourceTags := strings.Split(source.Tags, ",")
-	topics = append(topics, articleTags... )
-	topics = append(topics, sourceTags... )
+	topics = append(topics, articleTags...)
+	topics = append(topics, sourceTags...)
 	param.Topics = topics
 
 	w.Header().Add("Content Type", "text/html")
@@ -124,37 +126,77 @@ func (s *HttpServer) DisplayArticleById(w http.ResponseWriter, r *http.Request) 
 }
 
 type ListArticleParam struct {
-	Title string
+	Title    string
 	Subtitle string
-	Items *[]api.Article
+	Items   *[]ListArticlesDetailsParam
+}
+
+type ListArticlesDetailsParam struct {
+	Article api.Article
+	Source	api.Source
 }
 
 // This returns the newest 50 articles to the user
 // /articles/list
 func (s *HttpServer) ListArticles(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+
+	// Checking if the user is requesting the list view
+	hasTable := q.Has("table")
+	var tableValue string
+	if hasTable {
+		tableValue = q.Get("table")
+	}
+
 	param := ListArticleParam{
-		Title: "Newest Articles",
+		Title:    "Newest Articles",
 		Subtitle: "Below is a list of the newest articles pulled for you to view.",
 	}
 
-	items, err  := s.api.Articles.List()
+	items, err := s.api.Articles.List()
 	if err != nil {
 		w.Write([]byte(err.Error()))
 	}
 
-	param.Items = items
+	var details []ListArticlesDetailsParam
+
+	for _, item := range *items {
+		source, err  := s.api.Sources.GetById(item.Sourceid)
+		if err != nil {
+			log.Printf("Article '%v', has a invalid SourceID", item.ID)
+		}
+		var s api.Source
+		s = *source
+
+		d := ListArticlesDetailsParam{
+			Source: s,
+			Article: item,
+		}
+		details = append(details, d)
+	}
+
+	param.Items = &details
 
 	w.Header().Add("Content Type", "text/html")
-	err = s.templates.ExecuteTemplate(w, "articles.list", param)
-	if err != nil {
-		w.Write([]byte(err.Error()))
+	if hasTable && tableValue == "true" {
+		// Render the list format
+		err = s.templates.ExecuteTemplate(w, "articles.list.min", param)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
+	} else {
+		// Render the default format
+		err = s.templates.ExecuteTemplate(w, "articles.list", param)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
 	}
 }
 
 // /articles/sources/{ID}/list
 func (s *HttpServer) ListArticlesBySource(w http.ResponseWriter, r *http.Request) {
 	param := ListArticleParam{
-		Title: "Newest Articles",
+		Title:    "Newest Articles",
 		Subtitle: "Below is a list of the newest articles pulled for you to view.",
 	}
 
@@ -170,7 +212,25 @@ func (s *HttpServer) ListArticlesBySource(w http.ResponseWriter, r *http.Request
 		w.Write([]byte(err.Error()))
 	}
 
-	param.Items = items
+	var details []ListArticlesDetailsParam
+
+	for _, item := range *items {
+		source, err  := s.api.Sources.GetById(item.Sourceid)
+		if err != nil {
+			log.Printf("Article '%v', has a invalid SourceID", item.ID)
+		}
+		var s api.Source
+		s = *source
+
+		d := ListArticlesDetailsParam{
+			Source: s,
+			Article: item,
+		}
+
+		details = append(details, d)
+	}
+
+	param.Items = &details
 
 	w.Header().Add("Content Type", "text/html")
 	err = s.templates.ExecuteTemplate(w, "articles.list", param)
@@ -180,23 +240,23 @@ func (s *HttpServer) ListArticlesBySource(w http.ResponseWriter, r *http.Request
 }
 
 type ListArticleSourcesParam struct {
-	Title string
+	Title    string
 	Subtitle string
-	Items *[]ApiSourceOverload
+	Items    *[]ApiSourceOverload
 }
 
 // /articles/sources
 func (s *HttpServer) ListArticleSources(w http.ResponseWriter, r *http.Request) {
 	param := ListArticleSourcesParam{
-		Title: "Available News Sources",
+		Title:    "Available News Sources",
 		Subtitle: "Below are the enabled news sources to pick from.",
 	}
 
-	records, err  := s.api.Sources.List()
+	records, err := s.api.Sources.List()
 	if err != nil {
 		s.templates.ExecuteTemplate(w, "err", ErrParam{
 			Title: "Failed to fetch sources",
-			Code: 500,
+			Code:  500,
 			Error: err,
 		})
 		return
@@ -210,10 +270,10 @@ func (s *HttpServer) ListArticleSources(w http.ResponseWriter, r *http.Request) 
 		Topics = append(Topics, s.generateTopics(item.Tags)...)
 
 		details = ApiSourceOverload{
-			Item: item,
+			Item:   item,
 			Topics: Topics,
 		}
-		Items = append(Items, details )
+		Items = append(Items, details)
 	}
 	param.Items = &Items
 
