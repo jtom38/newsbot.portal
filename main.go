43c9aab6f4d4ceeff71fe9ea82a1df43d98f48dd
James Tombleson <luther38@gmail.com>
2022-11-27 15:41:30 -0800

    minor changes

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/jtom38/newsbot/portal/routes"
+	//"github.com/jtom38/newsbot/portal/routes"
 	"github.com/jtom38/newsbot/portal/services"
+	"github.com/jtom38/newsbot/portal/web"
 )
 
 func main() {
@@ -15,7 +16,8 @@ func main() {
 	c := services.NewConfigClient()
 	apiAddress := c.GetConfig(services.Config_API_Address)
 
-	server := routes.NewServer(&ctx, apiAddress)
+	//server := routes.NewServer(&ctx, apiAddress)
+	server := web.NewServer(ctx, apiAddress)
 
 	log.Print("Starting portal on http://localhost:8080")
 	err := http.ListenAndServe(":8080", server.Router)
